e4d1635ab1b3652bfec9e8c2ab6bafc5fd098713
Alan Tackett <alan.tackett@accre.vanderbilt.edu>
2014-03-06 10:01:25 -0600

    Added support for libunwind and gperftools for help with large memory usage with heavy multithreading

--- scripts/build-external.sh
+++ scripts/build-external.sh
@@ -7,7 +7,7 @@ export MAKE_ARGS="-j 10"
 
 cd build
 
-for x in zlib openssl Jerasure apr apr-util zeromq czmq; do
+for x in libunwind gperftools zlib openssl Jerasure apr apr-util zeromq czmq; do
    [ -d ${x}-[0-9]* ] || tar --no-same-owner --no-same-permissions -zxvf ../tarballs/${x}-[0-9]*gz
    cd ${x}-[0-9]*
    cat ../../scripts/${x}-build.sh | sed -e "s|PREFIX=/usr/local|PREFIX=${PREFIX}|" > my-build.sh
