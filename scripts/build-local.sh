cedc18d91a27f50b46779ddd3660b87cfca87539
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2015-06-12 11:51:23 -0500

    Moving from redmine to github

--- scripts/build-local.sh
+++ scripts/build-local.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-[ "${1}" == "" ] && echo "${0} /install/prefix [git user-id|tar]  **Default is to use tar**" && exit 1
+[ "${1}" == "" ] && echo "${0} /install/prefix [git|tar]  **Default is to use tar**" && exit 1
 
 export PREFIX=${1}
 
 from="tar"
 if [ "${2}" == "git" ] ; then
    from=git
-   [ "${3}" == "" ] && echo "Pulling project from GIT but missing user id!" && exit 1
-   user=${3}
 fi
 
 cd build
@@ -17,7 +15,7 @@ for p in toolbox gop ibp lio; do
   if [ ! -d ${p} ] ; then
      if [ "${from}" == "git" ]; then
         echo "Checking out repository.  May be asked for password..."
-        GIT_SSL_NO_VERIFY=true git clone https://${user}@redmine.accre.vanderbilt.edu/git/${p}.git
+        git clone git@github.com:accre/lstore-${p}.git
      else
         tar -zxvf ../tarballs/${p}.tgz
      fi
